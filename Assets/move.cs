b41c2f2e633907098ca9aa75c02611be2738c76c
Alexander Teichmann <teichmann.alex.mss@gmail.com>
2024-11-27 16:10:50 +0100

    copy from gitlab

---
+++ Assets/move.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] 
+    private float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * speed);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * speed );
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * speed);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * speed);
+  
+    }
+}
